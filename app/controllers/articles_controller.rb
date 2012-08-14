e0eed34fe5e8956dfe9cd8a716de79bbc3293bc7
Jim Weirich <jim.weirich@gmail.com>
2012-08-14 08:45:10 +0100

    Finished Iteration 1

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,30 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params[:article])
+    @article.save
+    redirect_to @article, notice: "Saved '#{@article.title}'"
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update_attributes(params[:article])
+    redirect_to @article, notice: "Updated '#{@article.title}'"
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ActiveRecord::Base
   attr_accessible :title, :body
+  validates_presence_of :title, :body
 end
